364d3df3fe05f7bdbf1f27b4a5bcc9e08990a28c
Imperial-lord <absatyaprakash01@gmail.com>
2022-08-19 00:20:50 +0530

    :sparkles: Update section 3 with kubernetes

--- blog/posts/index.js
+++ blog/posts/index.js
@@ -14,7 +14,7 @@ app.get('/posts', (req, res) => {
   res.send(posts);
 });
 
-app.post('/posts', async (req, res) => {
+app.post('/posts/create', async (req, res) => {
   const id = randomBytes(4).toString('hex');
   const { title } = req.body;
   posts[id] = {
@@ -23,7 +23,7 @@ app.post('/posts', async (req, res) => {
   };
 
   await axios
-    .post('http://localhost:4005/events', {
+    .post('http://event-bus-srv:4005/events', {
       type: 'PostCreated',
       data: { id, title },
     })
@@ -38,5 +38,6 @@ app.post('/events', (req, res) => {
 });
 
 app.listen(4000, () => {
-  console.log('Server is running on port 4000');
+  console.log('v1000');
+  console.log('Listening on 4000');
 });
